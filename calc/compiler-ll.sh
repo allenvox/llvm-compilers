d49c96bc710fa09dc2c7e868dd13cd3c599b58d6
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-09-06 12:52:09 +0700

    Add generating assembly (.s) artifact in ll script

--- calc/compiler-ll.sh
+++ calc/compiler-ll.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+# Output assembly code
+llc -o=bin/$1.s bin/$1.ll
+
 # Compiles LLVM IR (bytecode) into assembly language for a specified architecture
 llc -march=x86-64 -O3 -filetype=obj -relocation-model=pic -o=bin/$1.o bin/$1.ll
+
 # Linking
 clang -o bin/$1.x bin/$1.o src/rtcalc.c
