0cf59b12facd5ef3233f6ef212bb88da3c89752b
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-09-06 11:08:41 +0700

    Add building configuration

---
+++ calc/compiler-ll.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Compiles LLVM IR (bytecode) into assembly language for a specified architecture
+llc -march=x86-64 -O3 -filetype=obj -relocation-model=pic -o=bin/$1.o bin/$1.ll
+# Linking
+clang -o bin/$1.x bin/$1.o src/rtcalc.c
